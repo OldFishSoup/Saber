8bf9e0f5cec0352fa42350e53ea93acaa88192a2
OldFishSoup <57801876+OldFishSoup@users.noreply.github.com>
2020-01-02 20:38:20 -0500

    Create Saber.js

---
+++ Saber.js
@@ -0,0 +1,24 @@
+const { Client, Collection } = require("discord.js");
+const config = require("./config.json");
+const saber = new Client({ fetchAllMembers: true });
+const enmap = require("enmap");
+saber.commands = new Collection();
+saber.aliases = new Collection();
+saber.commandsUsed = 1;
+saber.messages = 1;
+
+
+/**
+ *   Database stuff
+ */
+const enmap = require("enmap");
+
+saber.levels = new enmap({ name: "levels" });
+saber.currency = new enmap({ name: "currency" });
+saber.userStats = new enmap({ name: "userstats" });
+saber.badges = new enmap({ name: "badges" });
+saber.inventory = new enmap({ name: "inventory" });
+saber.partners = new enmap({ name: "partners" });
+
+
+saber.login(config.saber.token);
